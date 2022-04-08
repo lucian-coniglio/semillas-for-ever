952b5ad0ebfda3a3dc3a430f33304554bd66620c
lucian-coniglio <89875842+lucian-coniglio@users.noreply.github.com>
2022-04-08 14:24:21 -0300

    act 1 hecha

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -1,37 +1,61 @@
 package ar.edu.unahur.obj2.semillas
 
 class Menta(var altura: Double, val anioSemilla: Int) {
+    fun toleranciaSol(): Int {
+        return 7
+    }
+
     fun espacio(): Double {
-        TODO("Te la debo, amigue...")
+        return altura + 1
     }
 
     fun daSemillas(): Boolean {
-        TODO("Colgue, che...")
+        return altura > 0.4 || esFuerte()
     }
 
     fun esFuerte(): Boolean {
-        TODO("Estas seguro que lo queres implementar así?")
+        return toleranciaSol() > 9
     }
 
 }
 
 class Soja(var altura: Double, val anioSemilla: Int) {
+
+    fun toleranciaSol(): Int {
+        if (altura < 0.5) {return 6}
+        else if (altura in 0.5..1.0) {return 8}
+        else {return 12}
+    }
+
     fun espacio(): Double {
-        TODO("Lee un poco más el enunciado...")
+        return altura / 2
     }
 
     fun daSemillas(): Boolean {
-        TODO("Dalee")
+        return esFuerte() || anioSemilla > 2007 && altura in 0.75..0.9
     }
 
     fun esFuerte(): Boolean {
-        TODO("Así me implementas mejor")
+        return toleranciaSol() > 9
     }
 }
 
-class Planta(var altura: Double, val anioSemilla: Int) {
-    fun espacio() {
-        TODO("Pasaron cosas")
+
+class Quinoa(var altura: Double, val anioSemilla: Int, var espacio: Double) {
+    fun toleranciaSol(): Int {
+        if (espacio < 0.3) {return 10}
+        else {return 7}
     }
 
+    fun espacio(): Double {
+        return altura / 2
+    }
+
+    fun daSemillas(): Boolean {
+        return esFuerte() || anioSemilla in 2001..2008
+    }
+
+    fun esFuerte(): Boolean {
+        return toleranciaSol() > 9
+    }
 }
